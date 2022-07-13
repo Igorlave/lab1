ef503ce7ef38b965c968daf471e8c2cd7ae6de7d
Igorlave <Masanoringuyen@gmail.com>
2022-07-13 18:51:43 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,8 @@
+
 const config = {
     app: {
-        port: 3000,
-    },
+        port: process.env.PORT || 3000,
+    }
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require("./app");
-const config = require("./config");
-
+const config = require("./app/config");
 const PORT = config.app.port;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
